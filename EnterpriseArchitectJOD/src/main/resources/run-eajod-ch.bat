d091950fd761a98cd3ad2ef6af9e3a3fbef78275
fransvanderlek <fransvander@gmail.com>
2016-10-24 21:50:35 +0100

    cleanup

--- EnterpriseArchitectJOD/src/main/resources/run-eajod-ch.bat
+++
@@ -1,5 +0,0 @@
-C:
-
-"C:\Program Files (x86)\Java\jdk1.8.0_77\bin\java.exe" -Djava.library.path="C:\Program Files (x86)\Sparx Systems\EA\Java API" -jar EnterpriseArchitectJOD-0.0.1-SNAPSHOT-jar-with-dependencies.jar eajod-ch.properties
-
-PAUSE
