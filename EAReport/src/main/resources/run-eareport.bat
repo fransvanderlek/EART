a285348cd93c68cbc08fd6162b97faa005648137
fransvanderlek <fransvander@gmail.com>
2016-12-26 10:54:12 +0100

    new batch runner

---
+++ EAReport/src/main/resources/run-eareport.bat
@@ -0,0 +1,5 @@
+C:
+CD C:\Users\flek\git\EnterpriseArchitectJOD\EnterpriseArchitectJOD\deploy
+"C:\Program Files (x86)\Java\jdk1.8.0_77\bin\java.exe" -Djava.library.path="C:\Program Files (x86)\Sparx Systems\EA\Java API" -jar EAReport.jar
+
+PAUSE
