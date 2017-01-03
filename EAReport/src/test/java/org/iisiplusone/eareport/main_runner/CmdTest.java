0e0b687e63f4251699059eb10a67bf624ed8cbdd
fransvanderlek <fransvander@gmail.com>
2017-01-03 23:20:54 +0100

    extended basic docx test.

--- EAReport/src/test/java/org/iisiplusone/eareport/main_runner/CmdTest.java
+++ EAReport/src/test/java/org/iisiplusone/eareport/main_runner/CmdTest.java
@@ -9,6 +9,8 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
 import org.iisiplusone.eareport.main_runner.EaDocShell;
 import org.iisiplusone.eareport.main_runner.EaReportProperties;
 import org.iisiplusone.eareport.main_runner.EaReportProperties.PropName;
@@ -17,7 +19,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CmdTest {
-	
+
 	File propertiesFile;
 	EaReportProperties properties;
 
@@ -42,9 +44,23 @@ public class CmdTest {
 
 		File createdFile = new File(properties.get(OutputFile));
 
-		Assert.assertTrue("File "+createdFile.getAbsolutePath()+"not found", createdFile.exists());
+		Assert.assertTrue("File " + createdFile.getAbsolutePath() + "not found", createdFile.exists());
+
+		XWPFDocument generatedDocument = new XWPFDocument(new FileInputStream(createdFile));
+		XWPFTable table = generatedDocument.getTables().get(0);
+
+		String[][] expectedTable = { { "ASSOCIATION_1", "Component_2", "Component_1", "association 1 notes" },
+				{ "ASSOCIATION_2", "Component_2", "Component_3", "association 2 notes" } };
+
+		for (int i = 0; i < 2; i++) {
+			for (int k = 0; k < 4; k++) {
+				Assert.assertEquals("Table value mismatch at row=" + (i + 1) + ", column=" + k, expectedTable[i][k],
+						table.getRow(i + 1).getCell(k).getText().trim());
+			}
+		}
 		
-		//TODO assert that a proper odt file was created (no load errors)
+		//@TODO: verify diagram images
+		//generatedDocument.getAllPictures()
 	}
 
 }
