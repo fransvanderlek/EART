906feff0188592465fec1aab8579a765cd238810
fransvanderlek <fransvander@gmail.com>
2017-01-14 00:02:21 +0100

    cleanup and adapted zip contents

--- EAReport/deploy/run-eareport.bat
+++ EAReport/deploy/run-eareport.bat
@@ -1,3 +1,6 @@
-"C:\Program Files (x86)\Java\jdk1.8.0_77\bin\java.exe" -Djava.library.path="C:\Program Files (x86)\Sparx Systems\EA\Java API" -jar EAReport.jar
+SET JAVA_32BIT_HOME=%JAVA_HOME%
+SET EA_JAVAAPI_PATH="C:\Program Files (x86)\Sparx Systems\EA\Java API"
+
+%JAVA_32BIT_HOME%\bin\java.exe -Djava.library.path=%EA_JAVAAPI_PATH% -javaagent:agents/aspectjweaver.jar -jar EAReport-0.0.1-SNAPSHOT-jar-with-dependencies.jar -classpath %EA_JAVAAPI_PATH%
 
 PAUSE
--- EAReport/src/main/resources/run-eareport.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files (x86)\Java\jdk1.8.0_77\bin\java.exe" -Djava.library.path="C:\Program Files (x86)\Sparx Systems\EA\Java API" -jar EAReport.jar
-
-PAUSE
